bdf823e6c72938fce272447100fcaae4a5785a4a
Гушул Роман <107499502+HushulRoman@users.noreply.github.com>
2023-05-06 14:07:00 +0300

    Add files via upload

---
+++ ЛБ№9/ЛБ№9 Завдання 1/ЛБ№9 Завдання 1.cpp
@@ -0,0 +1,59 @@
+﻿/*
+У цьому коді ми спочатку ввели число k, потім створили початковий вектор v.
+Далі ми використали функцію std::search_n з параметрами v.begin() та v.end() для того, щоб знайти перший набір із k підряд розташованих додатніх чисел.
+Цей пошук відбувається з використанням функціонального об'єкту, який перевіряє, що числа більше 0.
+
+Якщо такий набір знайдено, ми використовуємо ітератори для того, щоб дублювати його у векторі.
+Це відбувається за допомогою функції insert, яка вставляє копію набору між оригінальним набором та його копією.
+
+У кінці ми виводимо змінений вектор за допомогою циклу for.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Точка входу в програму.
+int main(int argc, char* argv[])
+{
+    int k;
+    //std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Початковий вектор
+
+    std::vector<int> v;
+    int n;
+
+    std::cout << "Enter the number of elements in the vector: ";
+    std::cin >> n;
+
+    std::cout << "Enter the elements of the vector: " << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cout << "#" << (i + 1) << " ";
+        int elem;
+        std::cin >> elem;
+        v.push_back(elem);
+    }
+
+    std::cout << "Enter K: ";
+    std::cin >> k;
+
+    // Шукаємо перший набір із K підряд розташованих додатніх чисел
+    auto it = std::search_n(v.begin(), v.end(), k, 1, [](int x, int y) { return x > 0 && y > 0; });
+
+    // Якщо знайдено такий набір
+    if (it != v.end()) {
+        // Обчислюємо ітератори на початок та кінець набору
+        auto first = it;
+        auto last = it + k;
+
+        // Дублюємо набір у векторі
+        v.insert(last, first, last);
+    }
+
+    // Виводимо вектор
+    for (int x : v) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ ЛБ№9/ЛБ№9 Завдання 2/ЛБ№9 Завдання 2.cpp
@@ -0,0 +1,55 @@
+﻿/*
+Пояснення коду:
+
+    Створюємо порожній вектор v та змінну n для збереження кількості елементів вектору, які будуть введені з клавіатури.
+    Виводимо запит на введення кількості елементів вектору та зчитуємо значення в змінну n.
+    Виводимо запит на введення елементів вектору та зчитуємо значення кожного елементу в циклі за допомогою функції push_back.
+    Створюємо множину s для збереження унікальних елементів вектору.
+    Видаляємо мінімальний та максимальний елементи з множини.
+    Копіюємо залишок елементів множини у порядку спадання у новий вектор result за допомогою алгоритму copy та зворотніх ітераторів.
+    Виводимо результат.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+// Точка входу в програму.
+int main(int argc, char* argv[])
+{
+    //vector<int> v = { 3, 6, 2, 9, 1, 8, 5 };
+
+    vector<int> v;
+    int n;
+
+    cout << "Enter the number of elements in the vector: ";
+    cin >> n;
+
+    cout << "Enter the elements of the vector: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "#" << (i + 1) << " ";
+        int elem;
+        cin >> elem;
+        v.push_back(elem);
+    }
+
+
+    set<int> s(v.begin(), v.end()); // створюємо множину для збереження унікальних елементів
+    s.erase(s.find(*min_element(v.begin(), v.end()))); // видаляємо мінімальний елемент
+    s.erase(s.find(*max_element(v.begin(), v.end()))); // видаляємо максимальний елемент
+
+    // копіюємо залишок елементів у порядку спадання у новий вектор
+    vector<int> result;
+    copy(s.rbegin(), s.rend(), back_inserter(result));
+
+    // виводимо результат
+    for (auto elem : result) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
